c439f8ecbed3c32b243fcd99032bdb7d5306716d
Aitor García <aitor.garcia@gmail.com>
2008-10-08 11:22:28 +0200

    MailUserObserver refactored to call user.create_default_folders

--- app/models/mail_user_observer.rb
+++ app/models/mail_user_observer.rb
@@ -1,14 +1,6 @@
 class MailUserObserver  < ActiveRecord::Observer
   observe :user
   def after_save(user)
-    if user.recently_activated?
-      Tog::Config["plugins.tog_mail.messages.default_folders"].each(" "){|folder_type|
-        folder_type.strip!
-        Folder.new(:name => folder_type,
-        :deletable => false,
-        :folder_type => folder_type,
-        :user => user).save!
-      } if Tog::Config["plugins.tog_mail.messages.default_folders"]
-    end
+    user.create_default_folders! if user.recently_activated?
   end
 end
