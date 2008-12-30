0bdfe36266c880154ab5926f21b0cd4ef76d7a6a
Alberto Molpeceres <alberto.molpeceres@gmail.com>
2008-12-30 13:45:01 +0100

    i18n

--- app/controllers/member/messages_controller.rb
+++ app/controllers/member/messages_controller.rb
@@ -1,67 +1,67 @@
 class Member::MessagesController < Member::BaseController
-
+ 
   if Rails::VERSION::STRING >= '2.2'
-    extend  ActionView::Helpers::SanitizeHelper::ClassMethods
+    extend ActionView::Helpers::SanitizeHelper::ClassMethods
     include ActionView::Helpers::SanitizeHelper
   else
     include ActionView::Helpers::SanitizeHelper
   end
-
+ 
   def show
     @message = current_user.get_message(params[:id])
     @message.read!
     respond_to do |format|
       format.html
-      format.xml  { render :xml => @message }
+      format.xml { render :xml => @message }
     end
   end
-
+ 
   def index
-    @folder   = (current_user.folders.find(params[:id]) unless params[:id].nil?) || current_user.inbox
+    @folder = (current_user.folders.find(params[:id]) unless params[:id].nil?) || current_user.inbox
     @messages = @folder.messages.paginate :page => params[:page]
-
+ 
     respond_to do |format|
       format.html
-      format.rss  { render :rss => @messages }
+      format.rss { render :rss => @messages }
     end
   end
-
+ 
   def new
     @from = current_user
     @to = User.active.find(params[:user_id]) if params[:user_id]
     respond_to do |format|
       format.html
-      format.xml  { render :xml => @message }
+      format.xml { render :xml => @message }
     end
   end
-
+ 
   def create
     to_user = User.active.find(params[:message][:to_user_id])
     @message = Message.new(
-    :from     => current_user,
-    :to       => to_user,
-    :subject  => sanitize(params[:message][:subject]),
-    :content  => sanitize(params[:message][:content])
+    :from => current_user,
+    :to => to_user,
+    :subject => sanitize(params[:message][:subject]),
+    :content => sanitize(params[:message][:content])
     )
     respond_to do |format|
       if @message.dispatch!
-        flash[:ok] = 'Message was successfully delivered.'
+        flash[:ok] = I18n.t("tog_mail.member.message_sent")
         format.html { redirect_back_or_default(member_messages_path) }
-        format.xml  { render :xml => @message, :status => :created, :location => member_message_path(:id => @message) }
+        format.xml { render :xml => @message, :status => :created, :location => member_message_path(:id => @message) }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @message.errors, :status => :unprocessable_entity }
+        format.xml { render :xml => @message.errors, :status => :unprocessable_entity }
       end
     end
   end
-
+ 
   def destroy
     message = Message.find(params[:id])
     folder_id = message.folder_id
     message.destroy
     redirect_to :action => 'index', :id => folder_id
   end
-
+ 
   def search
     @match ="%"+sanitize(params[:keyword])+"%"
     @msgs = Message.find_by_sql(["SELECT messages.id
@@ -75,13 +75,17 @@ class Member::MessagesController < Member::BaseController
       page.replace_html 'search', :partial => 'search', :object => @fol_msgs
     end
   end
-
+ 
   def reply
     @reply_to = current_user.received_messages.find params[:id]
     respond_to do |format|
       format.html
-      format.xml  { render :xml => @message }
+      format.xml { render :xml => @message }
     end
   end
-
+ 
 end
+
+
+
+
--- init.rb
+++ init.rb
@@ -1,5 +1,10 @@
 require_plugin 'tog_core'
 
+require "i18n" unless defined?(I18n)
+Dir[File.dirname(__FILE__) + '/locale/**/*.yml'].each do |file|
+  I18n.load_path << file
+end
+
 Tog::Plugins.settings :tog_mail, "messages.default_folders"   => "Inbox Outbox",
                                  "messages_list.size"         => "10"
 
